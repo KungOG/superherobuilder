0c796abd4cadd985e58d7167b1c8cfd906ad0224
Henrik Foogel <henrik.foogel@medieinstitutet.se>
2019-06-05 14:32:56 +0200

    Home started

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,19 @@
 <template>
   <main class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <section class="home-page-content-1">
+      <div class="home-page-content-text">
+        <h2>Build your own superman!</h2>
+        <p>Create your ultimate super hero with a fun name, look, powers and more!</p>
+      </div>
+      <div>
+      <a href class="btn">Start</a>
+      </div>
+    </section>
+    <section class="home-page-content-2">
+      <img src="../assets/img/Superman.png" alt="Superhero img" class="home-page-content-img">
+      <img src="../assets/img/Superman.png" alt="Superhero img" class="home-page-content-img">
+      <img src="../assets/img/Superman.png" alt="Superhero img" class="home-page-content-img">
+    </section>
     
   </main>
 </template>
