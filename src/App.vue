e62781c984b0f0621e0d3b3e27598c8a129dcbe3
Oscar Gustafsson <oscar-gustafsson@live.com>
2019-06-07 14:57:31 +0200

    merged, fixed some profile style and layout

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <div id="app">
     <div id="nav">
-        <span>
-      <p id="login">Login</p>
-        </span>
+      
+        <p id="login">Login</p> 
+     
       <div class="red-bar">
         <h1>SUPERHEROS</h1>
-              <!-- -->
-            <router-link to="/">Home</router-link> |
-      <router-link to="/builder">builder</router-link> | 
-      <router-link to="/power">power</router-link> | 
-      <router-link to="/contact">contact</router-link> |
-      <router-link to="/profile">profile</router-link>
-      <!-- -->
+        <!-- -->
+        <router-link to="/">Home</router-link> |
+        <router-link to="/builder">builder</router-link> | 
+        <router-link to="/power">power</router-link> | 
+        <router-link to="/contact">contact</router-link> |
+        <router-link to="/profile">profile</router-link>
+        <!-- -->
       </div>
     </div>
     <router-view/>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,6 +1,26 @@
 <template>
-    <main>
-        <h1>This is an Profile page</h1>
+    <main class="home">
+        <section class="headline">
+            <h1>YOUR SUPERHEROS</h1>
+        </section>
+        <section class="home-page-content-2">
+            <img src="../assets/img/Superman.png" alt="Superhero img" class="home-page-content-img">
+            <img src="../assets/img/Superman.png" alt="Superhero img" class="home-page-content-img">
+            <img src="../assets/img/Superman.png" alt="Superhero img" class="home-page-content-img">
+        </section>
+        <section >
+            <h3 class="showcast">Namn</h3>
+            <ul class="active">
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </section>
+        <section class="profile-btns">
+            <a href="" class="download-btn">DOWNLOAD</a>
+            <a href="" class="edit-btn">EDIT</a>
+            <a href="" class="delete-btn">DELETE</a>
+    </section>
     </main>
 </template>
 
@@ -9,3 +29,77 @@ export default {
     name: 'profile'
 }
 </script>
+<style>
+
+.home {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+
+}
+.headline {
+    display: flex;
+    justify-content: center;
+    margin-top: 4rem;
+    font-size: 1.75rem;
+}
+.showcast {
+    display: flex;
+    justify-content: center;
+    padding: 1rem;
+    background: #E54626 /* $orange; */
+}
+.active {
+    display: flex;
+    justify-content: space-evenly !important;
+    flex-direction: row;
+}
+.profile-btns {
+    display: flex;
+    justify-content: space-between;
+    flex-direction: row;
+    
+}
+.download-btn {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: green;
+    color: black;
+    text-decoration: none;
+    padding: 2.3rem;    
+
+    /* width: 282px;
+    height: 132px;
+    left: 70px;
+    top: 879px;  */
+}    
+.edit-btn {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: yellow;
+    color: black;
+    text-decoration: none;
+    padding: 2.3rem;
+
+    /* width: 282px;
+    height: 132px;
+    left: 70px;
+    top: 879px; */
+}    
+.delete-btn {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: red;
+    color: black;
+    text-decoration: none;
+    padding: 2.3rem;
+
+  /*   width: 282px;
+    height: 132px;
+    left: 70px;
+    top: 879px; */
+}    
+</style>
