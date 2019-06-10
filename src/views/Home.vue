58b194ee3bf7883c318847abd9f786777dd505c3
Emil <emilpet93@gmail.com>
2019-06-10 14:43:16 +0200

    Styled the contact page. Only responsive remains

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,7 @@
         <p>Create your ultimate super hero with a fun name, look, powers and more!</p>
       </div>
       <div>
-      <a href class="btn">Start</a>
+        <a class="btn">Start</a>
       </div>
     </section>
     <section class="home-page-content-2">
